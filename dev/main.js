eab7831053880f5ff7910a5baa4fe7b47f57f0de
Mertcan C <mertcan.cezik@fcbhealthreaktor.com>
2022-06-15 15:27:10 +0300

    removed fs, init function requires js object

--- dev/main.js
+++ dev/main.js
@@ -1,14 +1,6 @@
 import buip from "../index.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-console.log(__dirname);
-const JSONPATH = path.join(__dirname, "../example-segments.json");
-
+import segments from "../example-segments.json" assert { type: "json" };
 buip.init({
-  jsonpath: JSONPATH,
-  initialSegment: 1,
+  data: segments,
 });
-console.log(buip.currentSegment.customData);
+//console.log(buip.currentSegment);
--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-import fs from "fs";
-function getNextsBySegment(segment) {
+const buip = {
+  currentSegment: {},
+  data: {},
+  init: ({ data }) => {
+    data.segments.forEach((element) => {
+      element.nextSegments = () => _getNextsBySegment(element);
+    });
+    buip.data = data;
+    const currentSegment = data.segments.find(
+      (x) => x.id === data.initialSegment
+    );
+    if (!currentSegment)
+      throw new Error(
+        `Can't find initial segment with id of ${data.initialSegment}`
+      );
+    buip.initialSegment = data.initialSegment;
+    buip.currentSegment = currentSegment;
+    buip._init = buip.init;
+    buip.init = undefined;
+  },
+  nextSegments: () => {
+    const ids = [...buip.currentSegment.nexts];
+    ids.forEach((element, index) => {
+      ids[index] = buip.data.segments.find((x) => x.id === element);
+      ids[index].parentSegment = buip.currentSegment.id;
+    });
+    return ids;
+  },
+  getSegmentById(id) {
+    return buip.data.segments.find((x) => x.id === id);
+  },
+  selectSegment(id) {
+    const segment = buip.data.segments.find((x) => x.id === id);
+    buip.currentSegment = segment;
+  },
+};
+
+function _getNextsBySegment(segment) {
   const ids = [...segment.nexts];
   ids.forEach((element, index) => {
     ids[index] = buip.data.find((x) => x.id === element);
@@ -7,41 +43,5 @@ function getNextsBySegment(segment) {
   });
   return ids;
 }
-const buip = {
-  currentSegment: {},
-  data: {},
-  init: ({ jsonpath, initialSegment = 0 }) => {
-    let jsonstring = fs.readFileSync(jsonpath);
-    let obj = JSON.parse(jsonstring);
-    obj.forEach((element) => {
-      element.nextSegments = () => getNextsBySegment(element);
-    });
-
-    buip.jsonPath = jsonpath;
-    buip.data = obj;
-    const currentSegment = obj.find((x) => x.id === initialSegment);
-    if (!currentSegment)
-      throw new Error(
-        `Can't find initial segment with id of ${initialSegment}`
-      );
-    buip.initialSegment = initialSegment;
-    buip.currentSegment = currentSegment;
-  },
-  nextSegments: () => {
-    const ids = [...buip.currentSegment.nexts];
-    ids.forEach((element, index) => {
-      ids[index] = buip.data.find((x) => x.id === element);
-      ids[index].parentSegment = buip.currentSegment.id;
-    });
-    return ids;
-  },
-  getSegmentById(id) {
-    return buip.data.find((x) => x.id === id);
-  },
-  selectSegment(id) {
-    const segment = buip.data.find((x) => x.id === id);
-    buip.currentSegment = segment;
-  },
-};
 
 export default buip;
